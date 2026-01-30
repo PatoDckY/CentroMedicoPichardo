0815bb35f539234c677d74e13acb76e67cbf5aff
PatoDckY <jesusf1705dck@gmail.com>
2026-01-30 10:17:56 -0600

    errores v2

--- app/test-404/page.tsx
+++
@@ -1,7 +0,0 @@
-// app/test-404/page.tsx
-import { notFound } from 'next/navigation';
-
-export default function Test404Page() {
-  notFound(); // Esto activará la página 404
-  return <div>Este texto no se verá</div>;
-}
--- app/test-server-error/page.tsx
+++
@@ -1,6 +0,0 @@
-// app/test-server-error/page.tsx
-export default function TestServerErrorPage() {
-  // Esto causará un error 500 al renderizar
-  const invalidData: any = null;
-  return <div>{invalidData.property.noExiste}</div>;
-}
